66ed15777e020a8dbbec0956615358f773e4cd44
edox61 <63751822+edox61@users.noreply.github.com>
2021-01-15 20:07:50 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ErrorApp from "./ErrorApp";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorApp />
+    </div>
+  );
+}
+export default App;
