a1f7493227c990b290900d0cf00e259c103e271f
edox61 <63751822+edox61@users.noreply.github.com>
2021-01-15 20:55:06 -0300

    Listo

--- src/ProductApp.js
+++ src/ProductApp.js
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 
-function ProductApp() {
-  const [variable, setVariable] = useState("");
+const initialProduct = {
+  title: "Titulo",
+  description: "Descripcion"
+};
 
+function ProductApp() {
+  const [product, setProduct] = useState(initialProduct);
+
+  const updateProduct = (properties, value) => {
+    //  console.log(e);
+    setProduct({
+      ...product,
+      [properties]: value
+    });
+  };
   return (
     <div>
-      <h1>Product</h1>
+      <button onClick={() => updateProduct("title", "Nuevo Titulo")}>
+        update
+      </button>
+      <h1>{product.title}</h1>
+      <h1>{product.description}</h1>
+      <pre>{JSON.stringify({ product }, null, 2)}</pre>
     </div>
   );
 }
