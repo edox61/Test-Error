835837f5130f8bedd6151dc270a6588f1c3b395c
edox61 <63751822+edox61@users.noreply.github.com>
2021-01-15 20:08:57 -0300

    Error 1

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
